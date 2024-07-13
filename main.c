92ba97c8aaaa8a1f2668c8c079c9b628d3434b4a
Lucas Chaves <gabrielucas2008@hotmail.com>
2024-07-13 16:47:48 -0300

    fix: changing variables to unsigned long

--- main.c
+++ main.c
@@ -3,21 +3,21 @@
 #include <string.h>
 #include <limits.h>
 
-unsigned int frameSize = 0;
-unsigned int memorySize = 0;
-unsigned int numFrames = 0;
-unsigned int pageTableSize = 0;
-unsigned int globalTimestamp = 0;
-unsigned int numFreeFrames = 0;
+unsigned long int frameSize = 0;
+unsigned long int memorySize = 0;
+unsigned long int numFrames = 0;
+unsigned long int pageTableSize = 0;
+unsigned long int globalTimestamp = 0;
+unsigned long int numFreeFrames = 0;
 unsigned int readCount = 0;
 unsigned int writeCount = 0;
 unsigned int pageFaultsCount = 0;
 unsigned int replacetamentsCount = 0;
 
 typedef struct {
-    long frameNumber;
+    unsigned long int frameNumber;
     int valid;
-    long int lastAccess;
+    unsigned long int lastAccess;
 } PageTableEntry;
 
 char *concat(const char *s1, const char *s2)
@@ -40,22 +40,22 @@ long int getAddrOffset(long int pageSizeInByte)
     return offset;
 }
 
-void initializePageTable(PageTableEntry* pageTable, int* freeFrames) {
-    for (int i = 0; i < pageTableSize; i++) {
+void initializePageTable(PageTableEntry* pageTable, unsigned long int* freeFrames) {
+    for (unsigned long int i = 0; i < pageTableSize; i++) {
         pageTable[i].frameNumber = -1;
         pageTable[i].valid = 0;
         pageTable[i].lastAccess = 0;
     }
-    for (int i = 0; i < numFrames; i++) {
+    for (unsigned long int i = 0; i < numFrames; i++) {
         freeFrames[i] = i;
     }
 }
 
 long int findPageWithLru(PageTableEntry* pageTable) {
-    long int lruPage = -1;
-    unsigned int minTimestamp = INT_MAX;
+    unsigned long int lruPage = -1;
+    unsigned long int minTimestamp = INT_MAX;
 
-    for (int i = 0; i < pageTableSize; i++) {
+    for (unsigned long int i = 0; i < pageTableSize; i++) {
         if (pageTable[i].valid && pageTable[i].lastAccess < minTimestamp) {
             minTimestamp = pageTable[i].lastAccess;
             lruPage = i;
@@ -71,8 +71,8 @@ int processReplacement(PageTableEntry* pageTable, char* algorithm) {
 }
 
 
-long int translateAddress(PageTableEntry* pageTable, long int offset, long int virtualAddress, int* freeFrames, char* algorithm) {
-    long int pageNumber = virtualAddress >> offset;
+long int translateAddress(PageTableEntry* pageTable, long int offset, unsigned long int virtualAddress, unsigned long int* freeFrames, char* algorithm) {
+    unsigned long int pageNumber = virtualAddress >> offset;
     globalTimestamp++;
     //printf("CALLING translateAddress -> address: %x(%ld) / pageNumber: %ld / valid: %d / lastAccess: %d / frameNumber: %d\n", virtualAddress, virtualAddress, pageNumber, pageTable[pageNumber].valid, pageTable[pageNumber].lastAccess, pageTable[pageNumber].frameNumber);
     if (pageTable[pageNumber].valid) {
@@ -81,14 +81,14 @@ long int translateAddress(PageTableEntry* pageTable, long int offset, long int v
     } else {
         //Page fault
         pageFaultsCount++;
-        int frameNumber;
+        unsigned long int frameNumber;
         if(numFreeFrames > 0) {
             frameNumber = freeFrames[--numFreeFrames];
         }
         else{
             //Replacement
             replacetamentsCount++;
-            int replacedPage = processReplacement(pageTable, algorithm);
+            unsigned long int replacedPage = processReplacement(pageTable, algorithm);
             if(replacedPage == -1) {
                 printf("No valid page to replace, memory is full\n");
                 exit(1);
@@ -106,26 +106,25 @@ long int translateAddress(PageTableEntry* pageTable, long int offset, long int v
     }
 }
 
-unsigned char readMemory(PageTableEntry* pageTable, long int offset, long int virtualAddress, int* freeFrames, unsigned char* memory, char* algorithm) {
+unsigned char readMemory(PageTableEntry* pageTable, long int offset, unsigned long int virtualAddress, unsigned long int* freeFrames, unsigned char* memory, char* algorithm) {
     readCount++;
-    long physicalAddress = translateAddress(pageTable, offset, virtualAddress, freeFrames, algorithm);
-    long int page = virtualAddress >> offset;
+    unsigned long int physicalAddress = translateAddress(pageTable, offset, virtualAddress, freeFrames, algorithm);
     return memory[physicalAddress];
 }
 
-void writeMemory(PageTableEntry* pageTable, long int offset,  long int virtualAddress, int* freeFrames, unsigned char* memory, char* algorithm) {
+void writeMemory(PageTableEntry* pageTable, long int offset,  unsigned long int virtualAddress, unsigned long int* freeFrames, unsigned char* memory, char* algorithm) {
     writeCount++;
-    int physicalAddress = translateAddress(pageTable, offset, virtualAddress, freeFrames, algorithm);
+    unsigned long int physicalAddress = translateAddress(pageTable, offset, virtualAddress, freeFrames, algorithm);
     memory[physicalAddress] = 0xAB;
 }
 
 void initializeMemory(unsigned char* memory) {
-    for(long int i=0; i<memorySize; i++) {
+    for(unsigned long int i=0; i<memorySize; i++) {
         memory[i] = 0xAC;
     }
 }
 
-void processLog(PageTableEntry* pageTable, long int offset, int* freeFrames, unsigned char* memory, char* algorithm, char *filename) {
+void processLog(PageTableEntry* pageTable, unsigned long int offset, unsigned long int* freeFrames, unsigned char* memory, char* algorithm, char *filename) {
     char* filePath = concat("files/", filename);
 
     FILE *file = fopen(filePath, "r");
@@ -133,24 +132,25 @@ void processLog(PageTableEntry* pageTable, long int offset, int* freeFrames, uns
         perror("Error opening file");
         exit(1);
     }
-
+    unsigned long int count = 0;
     char line[256];
     while (fgets(line, sizeof(line), file)) {
-        unsigned int virtualAddress;
+       //printf("%d\n", count++);
+        unsigned long int virtualAddress;
         char operation;
 
-        if (sscanf(line, "%x %c", &virtualAddress, &operation) != 2) {
+        if (sscanf(line, "%lx %c", &virtualAddress, &operation) != 2) {
             fprintf(stderr, "Invalid line format: %s", line);
             exit(1);
         }
-
+        //printf("virtualAddress: %lu\n", virtualAddress);
         if (operation == 'R') {
             unsigned char value = readMemory(pageTable, offset, virtualAddress, freeFrames, memory, algorithm);
         } else {
             writeMemory(pageTable, offset, virtualAddress, freeFrames, memory, algorithm);
         }
     }
-
+    printf("test\n");
     fclose(file);
 }
 
@@ -179,11 +179,11 @@ int main(int argc, char *argv[])
     int addressSizeInBits = 32;
 
     unsigned int frameSizeInKB = atoi(argv[3]);
-    long int frameSizeInByte = frameSizeInKB * 1024;
+    unsigned int frameSizeInByte = frameSizeInKB * 1024;
     frameSize = frameSizeInByte;
 
     unsigned int memorySizeInKB = atoi(argv[4]);
-    long int memorySizeInByte = memorySizeInKB * 1024;
+    unsigned int memorySizeInByte = memorySizeInKB * 1024;
     memorySize = memorySizeInByte;
     
     int offsetInBits = getAddrOffset(frameSizeInByte);
@@ -195,7 +195,7 @@ int main(int argc, char *argv[])
     //printf("\n0\n");
     numFrames = memorySize/frameSize;
 
-    int* freeFrames = (int*) malloc(numFrames * sizeof(int));
+    unsigned long int* freeFrames = (unsigned long int*) malloc(numFrames * sizeof(unsigned long int));
     numFreeFrames = numFrames;
     //printf("\n1\n");
     initializeMemory(memory);
